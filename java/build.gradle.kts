0932f0c0395fcc88e524b9f5a0db435d9edc1a73
Julian Bühler <juli27@users.noreply.github.com>
2019-01-04 22:55:08 +0100

    small build config improvements and updated JUnit (5.3.1 -> 5.3.2)

--- java/build.gradle.kts
+++ java/build.gradle.kts
@@ -1,5 +1,10 @@
 plugins {
-    `java-library`
+  `java-library`
+}
+
+java {
+  sourceCompatibility = JavaVersion.VERSION_11
+  targetCompatibility = JavaVersion.VERSION_11
 }
 
 repositories {
@@ -7,9 +12,9 @@ repositories {
 }
 
 dependencies {
-    testImplementation("org.junit.jupiter:junit-jupiter-api:5.3.1")
-    testImplementation("org.junit.jupiter:junit-jupiter-params:5.3.1")
-    testRuntimeOnly("org.junit.jupiter:junit-jupiter-engine:5.3.1")
+  testImplementation("org.junit.jupiter:junit-jupiter-api:5.3.2")
+  testImplementation("org.junit.jupiter:junit-jupiter-params:5.3.2")
+  testRuntimeOnly("org.junit.jupiter:junit-jupiter-engine:5.3.2")
 }
 
 tasks.test {
