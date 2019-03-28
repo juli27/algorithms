378a1b74444de5fad16e331718c3720d9e6accb8
Julian Bühler <juli27@users.noreply.github.com>
2019-03-28 20:10:07 +0100

    Implement binary search for int arrays

--- java/src/main/java/searching/ArraySearching.java
+++ java/src/main/java/searching/ArraySearching.java
@@ -33,4 +33,32 @@ public final class ArraySearching {
 
     return OptionalInt.empty();
   }
+
+  /**
+   * Searches for the first occurrence of value in the given sorted array using binary search.
+   *
+   * @param array the sorted array to search through
+   * @param value the value to find
+   * @return An optional containing the index of the first occurrence of value or empty if not found
+   * @throws NullPointerException if array is null
+   */
+  public static OptionalInt binarySearch(int[] array, int value) {
+    Objects.requireNonNull(array);
+
+    int low = 0;
+    int high = array.length - 1;
+    while (low <= high) {
+      int mid = (low + high) / 2;
+
+      if (array[mid] == value) {
+        return OptionalInt.of(mid);
+      } else if (value < array[mid]) {
+        high = mid - 1;
+      } else {
+        low = mid + 1;
+      }
+    }
+
+    return OptionalInt.empty();
+  }
 }
--- java/src/test/java/searching/ArraySearchingTests.java
+++ java/src/test/java/searching/ArraySearchingTests.java
@@ -24,4 +24,22 @@ final class ArraySearchingTests {
     assertThat(ArraySearching.linearSearch(array, 99)).hasValue(9);
     assertThat(ArraySearching.linearSearch(array, 8)).hasValue(2);
   }
+
+  @Test
+  void testBinarySearch() {
+    assertThrows(NullPointerException.class, () -> ArraySearching.binarySearch(null, 0));
+
+    int[] array = {};
+    assertThat(ArraySearching.binarySearch(array, 0)).isEmpty();
+
+    array = new int[]{1};
+    assertThat(ArraySearching.binarySearch(array, 1)).hasValue(0);
+    assertThat(ArraySearching.binarySearch(array, 0)).isEmpty();
+
+    array = new int[]{-2, -1, 2, 4, 6, 8, 8, 8, 10, 44, 99};
+    assertThat(ArraySearching.binarySearch(array, 1)).isEmpty();
+    assertThat(ArraySearching.binarySearch(array, -2)).hasValue(0);
+    assertThat(ArraySearching.binarySearch(array, 99)).hasValue(10);
+    assertThat(ArraySearching.binarySearch(array, 8)).hasValue(5);
+  }
 }
