d6457eef9465c4cb353021a0cbdbd50aa84cccff
Julian Bühler <juli27@users.noreply.github.com>
2020-06-22 00:08:37 +0200

    [java] Update build script

--- java/build.gradle.kts
+++ java/build.gradle.kts
@@ -1,10 +1,14 @@
+import org.gradle.api.tasks.testing.logging.TestLogEvent.*
+
 plugins {
   `java-library`
 }
 
+version = "0.1.0"
+
 java {
-  sourceCompatibility = JavaVersion.VERSION_11
-  targetCompatibility = JavaVersion.VERSION_11
+  sourceCompatibility = JavaVersion.VERSION_14
+  targetCompatibility = JavaVersion.VERSION_14
 }
 
 repositories {
@@ -17,6 +21,15 @@ dependencies {
   testImplementation("com.google.truth.extensions:truth-java8-extension:1.0.1")
 }
 
-tasks.named<Test>("test") {
+tasks.withType<JavaCompile> {
+  options.isDeprecation = true
+  options.encoding = "UTF-8"
+}
+
+tasks.withType<Test> {
   useJUnitPlatform()
+
+  testLogging {
+    events = setOf(FAILED, SKIPPED)
+  }
 }
