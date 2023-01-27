783f535b5810f1b9834066669753493441578c70
Julian Bühler <699919+juli27@users.noreply.github.com>
2023-01-27 11:14:17 +0100

    java: Bump JDK and dependencies
    JDK: 15 -> 19
    JUnit: 5.8.2 -> 5.9.2
    jqwik: 1.6.5 -> 1.7.2

--- java/build.gradle.kts
+++ java/build.gradle.kts
@@ -11,20 +11,26 @@ repositories {
 }
 
 dependencies {
-  testImplementation("org.junit.jupiter:junit-jupiter-api:5.8.2")
-  testImplementation("org.junit.jupiter:junit-jupiter-params:5.8.2")
+  testImplementation("org.junit.jupiter:junit-jupiter-api:5.9.2")
+  testImplementation("org.junit.jupiter:junit-jupiter-params:5.9.2")
   testImplementation("com.google.truth:truth:1.1.3")
   testImplementation("com.google.truth.extensions:truth-java8-extension:1.1.3")
-  testImplementation("net.jqwik:jqwik-api:1.6.5")
+  testImplementation("net.jqwik:jqwik-api:1.7.2")
 
-  testRuntimeOnly("org.junit.jupiter:junit-jupiter-engine:5.8.2")
-  testRuntimeOnly("net.jqwik:jqwik-engine:1.6.5")
+  testRuntimeOnly("org.junit.jupiter:junit-jupiter-engine:5.9.2")
+  testRuntimeOnly("net.jqwik:jqwik-engine:1.7.2")
+}
+
+java {
+  toolchain {
+    languageVersion.set(JavaLanguageVersion.of(19))
+  }
 }
 
 tasks.withType<JavaCompile> {
   options.isDeprecation = true
   options.encoding = "UTF-8"
-  options.release.set(15)
+  options.release.set(19)
 }
 
 tasks.withType<Test> {
